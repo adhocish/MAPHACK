9b02b3778874977661ab4050c1aeadd7cbbc8fc5
Andy <andy.doan@ymail.com>
2015-09-27 10:23:50 -0400

    Modified en/decode to remove leading zeroes

--- src/dumbencode.py
+++ src/dumbencode.py
@@ -1,18 +1,18 @@
-__d = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','*','.',',',';',':','\'']
+__d = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','*','.',',',';','\'']
 
 def dumb_encode(s):
     output = ""
     for c in s:
         if c.isdigit():
-            output += '0' + c
+            output += '1' + c
         else:
-            output += str(__d.index(c))
+            output += str(__d.index(c) + 10)
     return output
 
 def dumb_decode(s):
     output = ""
     for i in xrange(0, len(s), 2):
-        output += __d[int(s[i:i+2])]
+        output += __d[int(s[i:i+2]) - 10]
     return output
 
 def test():
@@ -21,6 +21,8 @@ def test():
         rand_str = ''.join(random.SystemRandom().choice(__d) for i in range(30))
         temp = dumb_encode(rand_str)
         temp = dumb_decode(temp)
+        print rand_str
+        print temp
         if rand_str != temp:
             print "Test failed."
             return
