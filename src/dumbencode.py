da98f39b837d19af481a72c898ee7f34fd982509
Andy <andy.doan@ymail.com>
2015-09-27 05:23:03 -0400

    Added encoder/decoder

---
+++ src/dumbencode.py
@@ -0,0 +1,28 @@
+__d = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','*','.',',',';',':','\'']
+
+def dumb_encode(s):
+    output = ""
+    for c in s:
+        if c.isdigit():
+            output += '0' + c
+        else:
+            output += str(__d.index(c))
+    return output
+
+def dumb_decode(s):
+    output = ""
+    for i in xrange(0, len(s), 2):
+        output += __d[int(s[i:i+2])]
+    return output
+
+def test():
+    import random
+    for i in range(100):
+        rand_str = ''.join(random.SystemRandom().choice(__d) for i in range(30))
+        temp = dumb_encode(rand_str)
+        temp = dumb_decode(temp)
+        if rand_str != temp:
+            print "Test failed."
+            return
+    print "All tests passed."
+    return
